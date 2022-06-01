7078a3d7899e1517e5b0d7f0261c518c53d2de63
Pain d'epice Aka Kingder le Devtapeur <77408412+Kingder93400@users.noreply.github.com>
2022-06-01 10:54:54 +0200

    ajout config

--- resources/es_extended/clesx/cl_contextUI.lua
+++ resources/es_extended/clesx/cl_contextUI.lua
@@ -1,38 +1,42 @@
-local context = ContextUI:CreateMenu("Informations") 
+if ConfigUseHudContext == true then 
 
-ContextUI:IsVisible(context, function()
-        
-    ContextUI:Button("Métiers : "..ESX.PlayerData.job.label, nil, function(Selected)
-        if (Selected) then
+    local context = ContextUI:CreateMenu("Informations") 
+
+    ContextUI:IsVisible(context, function()
+            
+        ContextUI:Button("Métiers : "..ESX.PlayerData.job.label, nil, function(Selected)
+            if (Selected) then
+            end
+        end)
+
+        ContextUI:Button("Organisation : "..ESX.PlayerData.job2.label, nil, function(Selected)
+            if (Selected) then
+            end
+        end)
+
+        for i = 1, #ESX.PlayerData.accounts, 1 do
+            if ESX.PlayerData.accounts[i].name == 'money' then
+                ContextUI:Button("Argent liquide : ~g~"..ESX.Math.GroupDigits(ESX.PlayerData.accounts[i].money.."$"), nil, function(Selected)
+                end)
+            end
         end
+
+
+        for i = 1, #ESX.PlayerData.accounts, 1 do
+            if ESX.PlayerData.accounts[i].name == 'bank'  then
+                ContextUI:Button("Argent Banque : ~b~"..ESX.Math.GroupDigits(ESX.PlayerData.accounts[i].money.."~s~$"), nil, function(Selected)
+                end)
+            end
+        end
+
+
+        for i = 1, #ESX.PlayerData.accounts, 1 do
+            if ESX.PlayerData.accounts[i].name == 'black_money'  then
+                ContextUI:Button("Argent Sale : ~r~"..ESX.Math.GroupDigits(ESX.PlayerData.accounts[i].money.."~s~$"), nil, function(Selected)
+                end)
+            end
+        end
+
     end)
 
-    ContextUI:Button("Organisation : "..ESX.PlayerData.job2.label, nil, function(Selected)
-        if (Selected) then
-        end
-    end)
-
-    for i = 1, #ESX.PlayerData.accounts, 1 do
-        if ESX.PlayerData.accounts[i].name == 'money' then
-            ContextUI:Button("Argent liquide : ~g~"..ESX.Math.GroupDigits(ESX.PlayerData.accounts[i].money.."$"), nil, function(Selected)
-            end)
-        end
-    end
-
-
-    for i = 1, #ESX.PlayerData.accounts, 1 do
-        if ESX.PlayerData.accounts[i].name == 'bank'  then
-            ContextUI:Button("Argent Banque : ~b~"..ESX.Math.GroupDigits(ESX.PlayerData.accounts[i].money.."~s~$"), nil, function(Selected)
-            end)
-        end
-    end
-
-
-    for i = 1, #ESX.PlayerData.accounts, 1 do
-        if ESX.PlayerData.accounts[i].name == 'black_money'  then
-            ContextUI:Button("Argent Sale : ~r~"..ESX.Math.GroupDigits(ESX.PlayerData.accounts[i].money.."~s~$"), nil, function(Selected)
-            end)
-        end
-    end
-
-end)
+end
