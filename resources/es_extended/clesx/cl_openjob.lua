22b8669cc8dfcfcd47d3ccd6b4e2264123b8ae53
Pain d'epice Aka Kingder le Devtapeur <77408412+Kingder93400@users.noreply.github.com>
2022-06-11 15:57:29 +0200

    fix du f6 des EMS

--- resources/es_extended/clesx/cl_openjob.lua
+++ resources/es_extended/clesx/cl_openjob.lua
@@ -1,7 +1,7 @@
 Keys.Register('F6', 'Métier', 'Ouvrir le menu des métiers', function()
     if ESX.PlayerData.job ~= nil then
         if ESX.PlayerData.job.name == 'ambulance' then
-            menuf6ambulance()
+   			akhyfideletablettems()
         end
       end
 end) 
